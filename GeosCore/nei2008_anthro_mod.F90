6d8cd6415cd70528957a60bde3a8d091e7579a76
Katherine Travis <ktravis@login04.as.harvard.edu>
2015-02-17 17:49:49 -0500

    Add check so mask is called if biomass is turned off
    Signed-off-by: Katherine Travis <ktravis@login04.as.harvard.edu>

--- GeosCore/nei2008_anthro_mod.F90
+++ GeosCore/nei2008_anthro_mod.F90
@@ -3094,6 +3094,15 @@
       !--------------------------------------------------
       ! Allocate and zero arrays for emissions
       !--------------------------------------------------
+      
+      IF ( .not. Input_Opt%LBIOMASS ) THEN
+      ! allocate and read USA Mask
+         ALLOCATE( USA_MASK( IIPAR, JJPAR ), STAT=RC )
+         IF ( RC /= 0 ) CALL ALLOC_ERR( 'USA_MASK' )
+         USA_MASK = 0d0
+         
+         CALL READ_NEI2008_MASK
+      ENDIF
 
       ALLOCATE( CO( IIPAR, JJPAR, 3, 24 ), STAT=RC )
       IF ( RC /= 0 ) CALL ALLOC_ERR( 'CO' )
